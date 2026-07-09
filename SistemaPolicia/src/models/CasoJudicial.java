224dcaae267072f413c7b7ef5ed1b16560e92a52
Marcela <marcela.wojtasik@alu.inspt.utn.edu.ar>
2026-07-09 03:00:56 -0300

    Entities

--- SistemaPolicia/src/models/CasoJudicial.java
+++ SistemaPolicia/src/models/CasoJudicial.java
@@ -9,4 +9,41 @@ public class CasoJudicial {
     private String claveJuez;
     private boolean condenado;
     private int mesesCarcel; //0 si no hay condena
+
+    public CasoJudicial(String idAsalto, String claveJuez, boolean condenado, int mesesCarcel) {
+        this.idAsalto = idAsalto;
+        this.claveJuez = claveJuez;
+        this.condenado = condenado;
+        this.mesesCarcel = mesesCarcel;
+    }
+
+    public String getIdAsalto() {
+        return idAsalto;
+    }
+
+    public String getClaveJuez() {
+        return claveJuez;
+    }
+
+    public boolean isCondenado() {
+        return condenado;
+    }
+
+    public int getMesesCarcel() {
+        return mesesCarcel;
+    }
+
+    public void setClaveJuez(String claveJuez) {
+        this.claveJuez = claveJuez;
+    }
+
+    public void setCondenado(boolean condenado) {
+        this.condenado = condenado;
+    }
+
+    public void setMesesCarcel(int mesesCarcel) {
+        this.mesesCarcel = mesesCarcel;
+    }
+    
+    
 }
