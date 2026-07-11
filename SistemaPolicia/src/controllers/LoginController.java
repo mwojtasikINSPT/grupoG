d3b6cbaf032aae8b2aee25affe0c59444b74f7ce
Marcela <marcela.wojtasik@alu.inspt.utn.edu.ar>
2026-07-11 02:55:28 -0300

    Logins y Menu Ppal

--- SistemaPolicia/src/controllers/LoginController.java
+++ SistemaPolicia/src/controllers/LoginController.java
@@ -16,27 +16,26 @@ public class LoginController {
         this.menuController = new MenuController();
     }
     
-    public void procesarLogin(UsuarioLoginDTO loginDTO) throws Exception{
-        //validar datos vacios
+   public Usuario procesarLogin(UsuarioLoginDTO loginDTO) throws Exception {
+        // Validar datos vacíos
         if(loginDTO.getNombreUsuario().trim().isEmpty() || loginDTO.getPassword().trim().isEmpty()){
             throw new Exception("El usuario y la contraseña no pueden estar vacíos.");
         }
-    
-        //Hacemos la busqueda con el try catch
-        try{
-            //Buscar el usuario en el archivo .txt
+
+        try {
+            // Busco el usuario en el archivo .txt
             Usuario usuario = usuarioDAO.buscarPorId(loginDTO.getNombreUsuario());
-            
+
             if(usuario.getPassword().equals(loginDTO.getPassword())){
-                //Si coinciden
-                // armar el pase para menuController
-            }else{
-                throw new Exception("La contraseña es incorrecta. Intente nuevamente");
+                // Si coinciden, devolvemos la variable encontrada
+                return usuario;
+            } else {
+                throw new Exception("La contraseña es incorrecta. Intente nuevamente.");
             }
-        
-        }catch(ObjetoNoEncontradoException e){
-            throw new Exception("El usuario '" + loginDTO.getNombreUsuario() + "' no esta registrado.");
-        }catch (ErrorAlLeerException e){
+
+        } catch (ObjetoNoEncontradoException e) {
+            throw new Exception("El usuario '" + loginDTO.getNombreUsuario() + "' no está registrado.");
+        } catch (ErrorAlLeerException e) {
             throw new Exception("Error en el sistema: " + e.getMessage());
         }
     }
--- SistemaPolicia/src/views/Main.java
+++ SistemaPolicia/src/views/Main.java
@@ -1,35 +1,61 @@
 package views;
 
-// Punto de entrada del programa. Inicializa servicios y muestra el login.
-
 import controllers.LoginController;
 import dtos.UsuarioLoginDTO;
+import models.Usuario;
 
+// Punto de entrada del programa. Inicializa servicios y muestra el login.
 public class Main {
 
     public static void main(String[] args) {
         LoginController loginController = new LoginController();
-        UIHelper.mostrarTitulo("SISTEMA DE SEGURIDAD BANCARIA");
-        UIHelper.imprimirMensaje("Por favor, inicie sesión para continuar.");
         
-        boolean loginExitoso = false;
-        
-        //Bucle login, insiste hasta que ponga los datos correctos.
-        while(!loginExitoso){
-            System.out.println("\n--- INICIO DE SESIÓN ---");
-            String username = UIHelper.leerTexto("Usuario");
-            String password = UIHelper.leerTexto("Contraseña");
+        // 1. General - Si un usuario cierra sesión, el sistema vuelve a esta pantalla.
+        while (true) {
+            UIHelper.mostrarTitulo("SISTEMA DE SEGURIDAD BANCARIA");
+            UIHelper.imprimirMensaje("Por favor, inicie sesión para continuar.");
             
-            //Armamos el DTO
-            UsuarioLoginDTO loginDTO = new UsuarioLoginDTO(username, password);
-        
-            try{
-                //el controlador procesa el login. si sale bien llama al menuController
-                loginController.procesarLogin(loginDTO);
-                loginExitoso = true;
-            }catch(Exception e){
-                System.out.println("\n[LOGIN FALLIDO]" + e.getMessage());
-                System.out.println("Intente nuevamente.");
+            Usuario usuarioLogueado = null;
+            boolean loginExitoso = false;
+            
+            // 2. Verificacion credenciales válidas
+            while (!loginExitoso) {
+                System.out.println("\n--- INICIO DE SESIÓN ---");
+                String username = UIHelper.leerTexto("Usuario");
+                String password = UIHelper.leerTexto("Contraseña");
+                
+                UsuarioLoginDTO loginDTO = new UsuarioLoginDTO(username, password);
+            
+                try {
+                    // El controlador verifica y nos devuelve quién es
+                    usuarioLogueado = loginController.procesarLogin(loginDTO);
+                    loginExitoso = true;
+                } catch (Exception e) {
+                    System.out.println("\n[LOGIN FALLIDO] " + e.getMessage());
+                    System.out.println("Intente nuevamente.");
+                }
+            }
+            
+            // 3. Leo el rol y le doy la vista que corresponde
+            switch (usuarioLogueado.obtenerRol()) {
+                case ADMINISTRADOR:
+                    MenuAdministrador menuAdmin = new MenuAdministrador();
+                    menuAdmin.mostrarMenu(usuarioLogueado);
+                    break;
+                    
+                case INVESTIGADOR:
+                    MenuInvestigador menuInvestigador = new MenuInvestigador();
+                    menuInvestigador.mostrarMenu(usuarioLogueado);
+                    break;
+                    
+                case VIGILANTE:
+                    MenuVigilante menuVigilante = new MenuVigilante();
+                    menuVigilante.mostrarMenu(usuarioLogueado);
+                    break;
+                    
+                default:
+                    UIHelper.imprimirError("Rol de usuario no reconocido por el sistema.");
+                    break;
             }
         }
     }
