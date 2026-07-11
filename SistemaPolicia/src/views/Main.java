0d1c13bdda9af08bda5fe185f3eec7f249b87276
Marcela <marcela.wojtasik@alu.inspt.utn.edu.ar>
2026-07-11 03:40:35 -0300

    Corrijo Main - Salida del Sistema

--- SistemaPolicia/src/views/Main.java
+++ SistemaPolicia/src/views/Main.java
@@ -6,62 +6,71 @@ import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import models.Usuario;
 
+
 // Punto de entrada del programa. Inicializa servicios y muestra el login.
 public class Main {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        
+
         System.setOut(new PrintStream(System.out, true, "UTF-8")); //Objeto para q muestre caracteres ASCII, necesita exception
 
         LoginController loginController = new LoginController();
-        
+
         // 1. General - Si un usuario cierra sesión, el sistema vuelve a esta pantalla.
         while (true) {
             UIHelper.mostrarTitulo("SISTEMA DE SEGURIDAD BANCARIA");
             UIHelper.imprimirMensaje("Por favor, inicie sesión para continuar.");
-            
+
             Usuario usuarioLogueado = null;
             boolean loginExitoso = false;
-            
-            // 2. Verificacion credenciales válidas
+
+            // 2. Verifico credenciales válidas
             while (!loginExitoso) {
-                System.out.println("\n--- INICIO DE SESIÓN ---");
-                String username = UIHelper.leerTexto("Usuario");
+                UIHelper.mostrarTitulo("\n--- INICIO DE SESIÓN ---");
+                String username = UIHelper.leerTexto("Usuario (0 para Salir del Sistema)");
+
+                // Verifico si quiere salir
+                if (username.equals("0")) {
+                    UIHelper.imprimirMensaje("\nCerrando aplicación. ¡Hasta luego!");
+                    return;
+                }
+
                 String password = UIHelper.leerTexto("Contraseña");
-                
+
                 UsuarioLoginDTO loginDTO = new UsuarioLoginDTO(username, password);
-            
+
                 try {
                     // El controlador verifica y nos devuelve quién es
                     usuarioLogueado = loginController.procesarLogin(loginDTO);
                     loginExitoso = true;
                 } catch (Exception e) {
-                    System.out.println("\n[LOGIN FALLIDO] " + e.getMessage());
-                    System.out.println("Intente nuevamente.");
+                    UIHelper.imprimirError("\n[LOGIN FALLIDO] " + e.getMessage());
+                    UIHelper.imprimirMensaje("Intente nuevamente.");
                 }
             }
-            
+
             // 3. Leo el rol y le doy la vista que corresponde
             switch (usuarioLogueado.obtenerRol()) {
                 case ADMINISTRADOR:
                     MenuAdministrador menuAdmin = new MenuAdministrador();
                     menuAdmin.mostrarMenu(usuarioLogueado);
                     break;
-                    
+
                 case INVESTIGADOR:
                     MenuInvestigador menuInvestigador = new MenuInvestigador();
                     menuInvestigador.mostrarMenu(usuarioLogueado);
                     break;
-                    
+
                 case VIGILANTE:
                     MenuVigilante menuVigilante = new MenuVigilante();
                     menuVigilante.mostrarMenu(usuarioLogueado);
                     break;
-                    
-                default:
+
+                default: //En teoria, jamas llega hasta aca
                     UIHelper.imprimirError("Rol de usuario no reconocido por el sistema.");
                     break;
             }
         }
     }
+
 }
