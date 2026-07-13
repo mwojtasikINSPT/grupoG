e71c14635b87d812fa49f3da049dacd2cff19fe3
Marcela <marcela.wojtasik@alu.inspt.utn.edu.ar>
2026-07-13 02:44:54 -0300

    [DOCS] Javadoc y Jar en /dist

---
+++ SistemaPolicia/dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"controllers"},{"l":"daos"},{"l":"dtos"},{"l":"exceptions"},{"l":"models"},{"l":"views"},{"l":"views.submenues"}];updateSearchResults();
